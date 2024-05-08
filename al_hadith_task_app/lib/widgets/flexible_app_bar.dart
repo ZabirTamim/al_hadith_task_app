e6d79ce103ac81e9a9257670fa054df4723ee60c
xvrTamim <tamimazabir@gmail.com>
2024-05-08 22:36:52 +0600

    V 1.0.7
    small changes;

--- al_hadith_task_app/lib/widgets/flexible_app_bar.dart
+++ al_hadith_task_app/lib/widgets/flexible_app_bar.dart
@@ -6,26 +6,31 @@ import '../const/text_style_const.dart';
 class FlexibleAppBar extends StatelessWidget {
   final String titleText;
   final String subTitleText;
-  const FlexibleAppBar({
-    super.key, required this.titleText, required this.subTitleText,
-  });
 
+  const FlexibleAppBar({
+    super.key,
+    required this.titleText,
+    required this.subTitleText,
+  });
 
   @override
   Widget build(BuildContext context) {
     return FlexibleSpaceBar(
       background: Padding(
-        padding: const EdgeInsets.only(
-            left: 20, right: 20, bottom: 10, top: 20),
+        padding:
+            const EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Row(
               children: [
-                const Icon(
-                  Icons.arrow_back_ios,
-                  color: AlHadithColors.navTextWhite,
+                IconButton(
+                  onPressed: (){},
+                  icon: const Icon(
+                    Icons.arrow_back_ios,
+                    color: AlHadithColors.navTextWhite,
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(
@@ -35,8 +40,7 @@ class FlexibleAppBar extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(titleText,
-                          style: AlHadithTextStyle().headerText),
+                      Text(titleText, style: AlHadithTextStyle().headerText),
                       Text(
                         subTitleText,
                         style: AlHadithTextStyle().smallText,
@@ -46,10 +50,14 @@ class FlexibleAppBar extends StatelessWidget {
                 ),
               ],
             ),
-            const Icon(
-              CupertinoIcons.slider_horizontal_3,
-              color: AlHadithColors.navTextWhite,
-            )
+            IconButton(
+              onPressed: (){},
+              icon: const Icon(
+                CupertinoIcons.slider_horizontal_3,
+                color: AlHadithColors.navTextWhite,
+              ),
+            ),
+
           ],
         ),
       ),
